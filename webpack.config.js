e5fa280677b0eba2ea8dea131fc301f7ac956f04
Rick Ford <rickfordrick@gmail.com>
2018-07-19 14:08:35 -0500

    remove unneccesary comments

--- webpack.config.js
+++ webpack.config.js
@@ -2,21 +2,21 @@ const path = require('path');
 const autoprefixer = require('autoprefixer');
 
 module.exports = {
-  entry: './src/EasyModal.jsx',
-  output: {
-    path: path.resolve(__dirname, 'build'),
-    filename: 'index.js',
-    libraryTarget: 'commonjs2' // THIS IS THE MOST IMPORTANT LINE! :mindblow: I wasted more than 2 days until realize this was the line most important in all this guide.
+	entry: './src/EasyModal.jsx',
+	output: {
+		path: path.resolve(__dirname, 'build'),
+		filename: 'index.js',
+		libraryTarget: 'commonjs2'
 	},
 	mode: "production",
-  module: {
-    rules: [
-      {
-        test: /\.js?x$/,
-        include: path.resolve(__dirname, 'src'),
-        exclude: /(node_modules|build)/,
-        use: 'babel-loader'
-      },
+	module: {
+		rules: [
+			{
+				test: /\.js?x$/,
+				include: path.resolve(__dirname, 'src'),
+				exclude: /(node_modules|build)/,
+				use: 'babel-loader'
+			},
 			{
 				test: /\.s?css$/,
 				use: [
@@ -51,11 +51,11 @@ module.exports = {
 					}
 				]
 			}
-    ]
-  },
-  externals: {
-    'react': 'commonjs react' // this line is just to use the React dependency of our parent-testing-project instead of using our own React.
-  },
+		]
+	},
+	externals: {
+		'react': 'commonjs react'
+	},
 	resolve: {
 		extensions: ['.js', '.jsx']
 	}
